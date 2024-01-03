2d514b7ea90d22e144e07508899a69c7f59a6944
jeonghi <jpark0902@kookmin.ac.kr>
2024-01-04 08:53:14 +0900

    Initial Commit

---
+++ BMICalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMICalculator
+//
+//  Created by 쩡화니 on 1/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
